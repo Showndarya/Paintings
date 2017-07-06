236dc6df641ebbf208d016653f6e0db0e1b6ce13
mushu <showndarya.m@somaiya.edu>
2017-07-06 17:19:56 +0530

    Added likes

--- app/src/main/java/com/example/mushu/testintents/Abst.java
+++ app/src/main/java/com/example/mushu/testintents/Abst.java
@@ -9,20 +9,79 @@ import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 import android.widget.ViewSwitcher;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class Abst extends AppCompatActivity {
 
     private Integer images[] = {R.drawable.a1, R.drawable.a2, R.drawable.a3,R.drawable.a4,R.drawable.a5};
     private int currImage = 0;
+    Button like;
 
-
+    DatabaseReference databaseLikes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_abst);
+        like = (Button) findViewById(R.id.like);
 
         initializeImageSwitcher();
         setInitialImage();
         setImageRotateListener();
+        setLike();
+        databaseLikes = FirebaseDatabase.getInstance().getReference("abstlikes");
+
+    }
+
+    private void setLike(){
+        like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             final String id = Integer.toString(currImage);
+            /* databaseLikes.child("abstlikes").child(id).addListenerForSingleValueEvent(new ValueEventListener() {
+                  @Override
+                  public void onDataChange(DataSnapshot dataSnapshot) {
+                      Likes l = dataSnapshot.getValue(Likes.class);
+                      int likes = l.getLikes();
+                      likes++;
+                      Likes likesin = new Likes(likes);
+                      databaseLikes.child(id).setValue(likesin);
+                  }
+
+                  @Override
+                  public void onCancelled(DatabaseError databaseError) {
+
+                  }
+              });*/
+                databaseLikes.child(id).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                      //  for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                           // if(dataSnapshot.getKey()==id) {
+                                Likes l = dataSnapshot.getValue(Likes.class);
+                                int cnt = l.getLikes();
+                                cnt = cnt + 1;
+                                Likes likecnt = new Likes(cnt);
+                                databaseLikes.child(id).setValue(likecnt);
+                                //break;
+                            //}
+                        //}
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+             //  Likes likesin = new Likes(2);
+             //  databaseLikes.child(id).setValue(likesin);
+            }
+        });
+
 
     }
 
---
+++ app/src/main/java/com/example/mushu/testintents/Likes.java
@@ -0,0 +1,28 @@
+package com.example.mushu.testintents;
+
+/**
+ * Created by mushu on 7/6/17.
+ */
+
+public class Likes {
+
+    int likes;
+
+    public Likes(){
+
+    }
+
+    public Likes( int likes) {
+
+        this.likes = likes;
+    }
+
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+}
